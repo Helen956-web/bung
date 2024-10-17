6ae25b5d3cf51761a3e6db489c34fb7036148da5
Helen956-web <helen.mueller@thg-hn.de>
2024-10-17 11:07:36 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 let strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
 strip.setBrightness(50)
-strip.setPixelWhiteLED(1, 2)
 basic.forever(function () {
     for (let Index = 0; Index <= 29; Index++) {
         strip.setPixelColor(Index, neopixel.colors(NeoPixelColors.White))
